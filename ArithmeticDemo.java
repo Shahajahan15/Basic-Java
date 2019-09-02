bcb5a60e958592ae875e683eddf2f60e7e84a93b
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-02 21:25:26 +0600

    Arithmetic Operation

---
+++ ArithmeticDemo.java
@@ -0,0 +1,33 @@
+package basicjava;
+
+import java.util.Scanner;
+
+/**
+ * @author Shahajahan
+ */
+public class ArithmeticDemo {
+    public static void main(String[] arge) {
+        int num1, num2, result;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the first number : ");
+        num1 = input.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = input.nextInt();
+        
+        result = num1 + num2;
+        System.out.println("Sum : "+result);
+        
+        result = num1 - num2;
+        System.out.println("Sub : "+result);
+        
+        result = num1 * num2;
+        System.out.println("Multipolication : "+result);
+        
+        double result2 = (double)num1 / num2;
+        System.out.println(0.2f+"Devide : "+result2);
+        
+        result = num1 % num2;
+        System.out.println("Remainder : "+result);
+    }
+}
