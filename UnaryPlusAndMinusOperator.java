df284892dabe87a18da22839ecdcc904eec4353b
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-03 20:19:17 +0600

    Unary Plus And Minus Operator

---
+++ UnaryPlusAndMinusOperator.java
@@ -0,0 +1,16 @@
+package basicjava;
+
+public class UnaryPlusAndMinusOperator {
+    
+    public static void main(String[] args) {
+        int x = -10;
+        int result;
+        
+        result = +x;
+        System.out.println("Out put : "+result);
+        
+        result = -x;
+        System.out.println("Out put : "+result);
+    }
+ 
+}
