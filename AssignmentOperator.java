2317df908b335215078b0d95dda47605ebb6faad
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-03 15:28:26 +0600

    Assignment Operator

---
+++ AssignmentOperator.java
@@ -0,0 +1,27 @@
+package basicjava;
+/**
+ * @author Shahajahan
+ */
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4;
+        
+        x+=y;  // x = x+y = 9;
+        System.out.println("X is : " + x);
+        
+        x-=y;  // x = x-y = 5;
+        System.out.println("X is : " + x);
+        
+        x*=y;  // x = x*y = 20;
+        System.out.println("X is : " + x);
+        
+        x/=y;  // x = x/y = 5;
+        System.out.println("X is : " + x);
+        
+        x%=y;  // x = x%y = 1;
+        System.out.println("X is : " + x);
+        
+        
+    }
+}
