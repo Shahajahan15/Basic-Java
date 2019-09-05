85dfd435c77500340efefa3912fcb9d68f52a4a2
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-05 17:32:23 +0600

    Checking Vowel / Consonant Leter

---
+++ VowelConsonant.java
@@ -0,0 +1,37 @@
+package basicjava;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+    
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+        char ch;
+        System.out.print("Enter any Leter : ");
+        ch = input.next().charAt(0);
+        if (ch == 'a' || ch == 'A') 
+        {
+            System.out.println("Vowel");
+        }
+        else if (ch == 'e' || ch == 'E') 
+        {
+            System.out.println("Vowel");
+        }
+        else if (ch == 'i' || ch == 'I') 
+        {
+            System.out.println("Vowel");
+        }
+        else if (ch == 'o' || ch == 'O') 
+        {
+            System.out.println("Vowel");
+        }
+        else if (ch == 'u' || ch == 'U') 
+        {
+            System.out.println("Vowel");
+        }
+        else
+        {
+            System.out.println("Consonant");
+        }
+    }
+}
