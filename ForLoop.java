4d0f0ead8788fce5c120f2ab3202eef0c68c2cdd
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-07 13:47:45 +0600

    For Loop

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+package basicjava;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Bangladesh "+ i);
+        }
+    }
+}
