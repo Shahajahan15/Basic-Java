530c830308646bd6a83f7abbe1522316b391b19f
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-07 14:08:53 +0600

    Do While Loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,15 @@
+package basicjava;
+
+public class DoWhileLoop {
+    
+    public static void main(String [] args)    
+    {
+        int i = 1;
+        
+        do {
+            System.out.println(i);
+            i = i + 2;
+        } 
+        while(i <= 50);
+    }
+}
